2725065a13a5ce9679ba20c97125cc1b14486a93
AxelMaccarini <axel.maccarini@mapsgroup.it>
2022-12-11 15:01:55 +0100

    v 1.0.0

--- src/app.js
+++ src/app.js
@@ -1,4 +1,32 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const cors = require('cors')();
 
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('sendMessage', (msg) => {
+        console.log(msg);
+
+      socket.emit('nuovoMessagio', msg);
+      socket.broadcast.emit('nuovoMessagio', msg);
+
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
