decdc1766b93a1afa83107fb1bc8c3c28ef0c085
AxelADMIN <axelghigo@gmail.com>
2023-10-18 23:59:25 +0200

    fix cors

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: '*',
+    origin: 'https://totem-socket.adaptable.app/',
     methods: ["GET", "POST"]
   }
 });
