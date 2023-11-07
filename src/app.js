fb9d9270a53402b4334618897cf01a8f7f25e49d
AxelADMIN <axelghigo@gmail.com>
2023-11-07 23:57:42 +0100

    add body.json

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
+const newObj = require('./body.json');
 const express = require('express');
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
-const newObj = {
-  "head": ["priorità", "type", "number", "data", "room", "stato"],
-  "body": [[{ "value": "alta" }, { "png": "sedia", "class": "type" }, { "value": "NP001", "color": "red", "class": "standard" }, { "value": "9 ago 17:15" }, { "value": "Piano terra" }, { "value": "Chiamato", "class": "blink " }],
-  [{ "value": "alta" }, "", { "value": "NP002", "color": "red", "class": "standard" }, { "value": "9 ago 17:15" }, { "value": "Piano terra" }, { "value": "Chiamato", "class": "blink " }],
-  [{ "value": "bassa" }, { "value": "" }, { "value": "NP003", "color": "green", "class": "standard" }, { "value": "9 ago 17:15" }, { "value": "Piano terra" }, { "value": "Chiamato", "class": "blink " }],
-  [{ "value": "media" }, "", { "value": "NP004", "color": "orange", "class": "standard" }, { "value": "9 ago 17:15" }, { "value": "Piano terra" }, { "value": "Chiamato", "class": "blink " }],
-  [{ "value": "media" }, "", { "value": "NP005", "color": "", "class": "standard" }, { "value": "9 ago 17:15" }, { "value": "Piano terra" }, { "value": "Chiamato", "class": "blink " }]]
-}
-
-
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
   cors: {
-    origin: ["https://corso-angular-f932b.web.app"]
+    origin: "*"
   }
 });
 
@@ -26,8 +17,8 @@ app.get('/', (req, res) => {
   res.end()
 })
 
-
 let user = []
+
 
 io.on('connection', async (socket) => {
   console.log('a user connected ' + socket.id);
@@ -53,7 +44,6 @@ io.on('connection', async (socket) => {
       data: msg
 
     });
-
 
   });
   //invio tab selezionato
